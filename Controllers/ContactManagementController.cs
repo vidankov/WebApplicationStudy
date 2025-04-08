2b969b0e22e2e3e0081179b795e1ae0f9849bd1b
vidankov <slavavinsent@gmail.com>
2025-04-08 13:13:40 +0300

    Реализован метод добавления контактов

---
+++ Controllers/ContactManagementController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ContactManagementController : BaseController
+{
+    private readonly ContactStorage _storage;
+
+    public ContactManagementController(ContactStorage storage)
+    {
+        _storage = storage;
+    }
+
+    [HttpPost("contacts")]
+    public void Create([FromBody] Contact contact)
+    {
+        _storage.Contacts.Add(contact);
+    }
+}
