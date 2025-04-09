f5b08261ea3e76033f0b8a67a55d7d5fd30bc3a6
vidankov <slavavinsent@gmail.com>
2025-04-09 13:19:32 +0300

    Добавлено свойство Phone в модель хранения контактов; Реализовано изначальное наполнение хранилища контактов тестовыми фейковыми данными

--- Storage/ContactStorage.cs
+++ Storage/ContactStorage.cs
@@ -1,9 +1,23 @@
-﻿public class ContactStorage
+﻿using Bogus;
+
+public class ContactStorage
 {
     public List<Contact> Contacts = [];
 
     public ContactStorage()
     {
         Contacts = [];
+
+        int ids = 1;
+        var faker = new Faker<Contact>("ru")
+            .RuleFor(c => c.Id, f => ids++)
+            .RuleFor(c => c.Name, f => f.Name.FullName())
+            .RuleFor(c => c.Phone, f => f.Phone.PhoneNumber("7-###-###-####"))
+            .RuleFor(c => c.Email, f => f.Internet.Email());
+
+        for (int i = 0; i < 5; i++)
+        {
+            Contacts.Add(faker.Generate());
+        }
     }
 }
