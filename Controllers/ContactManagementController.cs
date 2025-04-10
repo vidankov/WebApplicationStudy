a3e221f4f8ac43717573d06c9cd6e1f2aadde9e5
vidankov <slavavinsent@gmail.com>
2025-04-10 13:35:52 +0300

    Реализован метод для редактирования контактов

--- Controllers/ContactManagementController.cs
+++ Controllers/ContactManagementController.cs
@@ -15,15 +15,34 @@ public class ContactManagementController : BaseController
         _storage.Contacts.Add(contact);
     }
 
-    [HttpGet("contacts")]
-    public List<Contact> GetContacts() 
-    {
-        return _storage.Contacts;
-    }
-
     [HttpDelete("contacts/{id}")]
     public void Delete(int id) 
     {
         _storage.Contacts.Remove(_storage.Contacts.First(c => c.Id == id));
+    }
+
+    [HttpPut("contacts/{id}")]
+    public void Update(int id, ContactDto contactDto)
+    {
+        var contact = _storage.Contacts.First(c => c.Id == id);
+        contact.Email = ValidateNewValue(contactDto.Email) ? contactDto.Email : contact.Email;
+        contact.Phone = ValidateNewValue(contactDto.Phone) ? contactDto.Phone : contact.Phone;
+        contact.FirstName = ValidateNewValue(contactDto.FirstName) ? contactDto.FirstName : contact.FirstName;
+        contact.LastName = ValidateNewValue(contactDto.LastName) ? contactDto.LastName : contact.LastName;
+    }
+
+    [HttpGet("contacts")]
+    public List<Contact> GetContacts()
+    {
+        return _storage.Contacts;
+    }
+
+    protected bool ValidateNewValue(string? value)
+    {
+        if (string.IsNullOrEmpty(value) || value == "string")
+        {
+            return false;
+        }
+        return true;
     }
 }
