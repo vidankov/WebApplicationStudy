9647f1ffb57679df70d979ef2d5a478b4478048e
vidankov <slavavinsent@gmail.com>
2025-04-18 12:46:25 +0300

    Подключён пакет для работы с Sqlite, создан класс SqliteStorage

---
+++ Storage/SqliteStorage.cs
@@ -0,0 +1,25 @@
+﻿
+public class SqliteStorage : IStorage
+{
+    public List<Contact> Contacts => throw new NotImplementedException();
+
+    public bool Add(ContactDto contactDto, out Contact contact)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Contact? GetContactById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(int id, ContactDto contactDto, out Contact? contact)
+    {
+        throw new NotImplementedException();
+    }
+}
