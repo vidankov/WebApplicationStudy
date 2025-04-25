25b4b10c240ae623cfc15169babadbb6f25a187f
vidankov <slavavinsent@gmail.com>
2025-04-25 14:26:09 +0300

    Добавлены пакеты для работы с EF.

---
+++ DataContext/SqliteDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+public class SqliteDbContext : DbContext
+{
+    public DbSet<Contact> contacts { get; set; }
+
+    public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options) { }
+}
